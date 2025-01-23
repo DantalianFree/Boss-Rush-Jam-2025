ddb0e6e2bd5729552a01c551d7be3d6af284a535
DantalianFree <mdrojo.chmsu@gmail.com>
2025-01-23 23:58:18 +0800

    changes

--- boss-rush-jam/Scripts/Character/player.gd
+++ boss-rush-jam/Scripts/Character/player.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
-@onready var animated_sprite = $Maharlikav1
 
-const SPEED = 300
+@onready var animation_player = $Sprite2D/AnimationPlayer
+
+const SPEED = 200
+
+var is_attacking = false 
 
 func _process(delta):
 	var direction = Vector2.ZERO
+
+	if !is_attacking:
+		update_animation_by_mouse()
+		
+	# Handle movement input
 	if Input.is_action_pressed("Walk_Right"):
 		direction.x += 1
 	if Input.is_action_pressed("Walk_Left"):
@@ -14,18 +22,33 @@ func _process(delta):
 	if Input.is_action_pressed("Walk_Up"):
 		direction.y -= 1
 	velocity = direction.normalized() * SPEED
-	
-	if direction != Vector2.ZERO:
-		animated_sprite.play()
-		if direction.y > 0:
-			animated_sprite.animation = "front_run"
-		elif direction.y < 0:
-			animated_sprite.animation = "back_run"
-		elif direction.x > 0:
-			animated_sprite.animation = "right_run"
-		elif direction.x <0:
-			animated_sprite.animation = "left_run"
-	else:
-		animated_sprite.animation = "Idle"
-		
+
+
 	move_and_slide()
+
+func update_animation_by_movement(direction: Vector2):
+	if abs(direction.y) > abs(direction.x):
+		if direction.y > 0:
+			pass
+		else:
+			pass
+	else:
+		if direction.x > 0:
+			pass
+		else:
+			pass
+
+func update_animation_by_mouse():
+	var mouse_position = get_global_mouse_position()
+	var direction_to_mouse = (mouse_position - global_position).normalized()
+
+	if abs(direction_to_mouse.y) > abs(direction_to_mouse.x):
+		if direction_to_mouse.y > 0:
+			animation_player.play("front_idle")
+		else:
+			animation_player.play("back_idle")
+	else:
+		if direction_to_mouse.x > 0:
+			animation_player.play("right_idle")
+		else:
+			animation_player.play("left_idle")
